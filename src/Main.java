60db244e2ed05e7c05e1ea9f59e4b2bc21303362
GryshkoRuslan <sempay1313@gmail.com>
2023-10-17 18:39:27 +0300

    hw-05

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SecondClass secondClass = new SecondClass();
+        secondClass.whatIsASeason();
+        secondClass.SumOfNumbersWhichWereDevidedTo3();
+    }
+}
